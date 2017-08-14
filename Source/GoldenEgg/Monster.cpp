aeaff7e3a88fa081295402fe3d1918abe2687fd2
MichaelCJHurst <mhurst93@live.co.uk>
2017-08-14 21:18:34 +0100

    added monster class

--- Source/GoldenEgg/Monster.cpp
+++ Source/GoldenEgg/Monster.cpp
@@ -4,11 +4,24 @@
 
 
 // Sets default values
-AMonster::AMonster()
+AMonster::AMonster(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// Set the defaults
+	Speed = 20;
+	HitPoints = 20;
+	Experience = 0;
+	BPLoot = NULL;
+	BaseAttackDamage = 1;
+	AttackTimeout = 1.5f;
+	TimeSinceLastStrike = 0;
+	// Sight sphere
+	SightSphere = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("SightSphere"));
+	SightSphere->AttachTo(RootComponent);
+	// Attack range sphere
+	AttackRangeSphere = ObjectInitializer.CreateDefaultSubobject<USphereComponent>(this, TEXT("AttackRangeSphere"));
+	AttackRangeSphere->AttachTo(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -24,11 +37,3 @@ void AMonster::Tick(float DeltaTime)
 	Super::Tick(DeltaTime);
 
 }
-
-// Called to bind functionality to input
-void AMonster::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-}
-
--- Source/GoldenEgg/Monster.h
+++ Source/GoldenEgg/Monster.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
 #include "GameFramework/Character.h"
 #include "Monster.generated.h"
 
@@ -13,19 +14,40 @@ class GOLDENEGG_API AMonster : public ACharacter
 
 public:
 	// Sets default values for this character's properties
-	AMonster();
+	AMonster(const FObjectInitializer& ObjectInitializer);
 
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
 public:	
+	// Speed of the baddie
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		float Speed;
+	// The health of the baddie
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		float HitPoints;
+	// XP gained upon death
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		int32 Experience;
+	// Blueprint of the loot drop
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		UClass* BPLoot;
+	// The damage that attacks do
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		float BaseAttackDamage;
+	// Time between attacks
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = MonsterProperties)
+		float AttackTimeout;
+	// Time since the last attack, viewable in blueprints
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = MonsterProperties)
+		float TimeSinceLastStrike;
+	// Sight range
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Collision)
+		USphereComponent* SightSphere;
+	// Attack range
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = Collision)
+		USphereComponent* AttackRangeSphere;
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-
-	
-	
 };
