9db72664222afd81508ef43a8eb42b1ef15eb82f
MichaelCJHurst <mhurst93@live.co.uk>
2017-08-18 15:56:54 +0100

    Added code for melee weapon

--- Source/GoldenEgg/MeleeWeapon.cpp
+++ Source/GoldenEgg/MeleeWeapon.cpp
@@ -8,7 +8,17 @@ AMeleeWeapon::AMeleeWeapon(const FObjectInitializer& ObjectInitializer) : Super(
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	// Set the defaults
+	AttackDamage = 1;
+	Swinging = false;
+	WeaponHolder = NULL;
+	// Set the mesh for this object
+	Mesh = ObjectInitializer.CreateDefaultSubobject<UStaticMeshComponent>(this, TEXT("Mesh"));
+	RootComponent = Mesh;
+	// Set the collision up
+	ProxBox = ObjectInitializer.CreateDefaultSubobject<UBoxComponent>(this, TEXT("ProxBox"));
+	ProxBox->OnComponentBeginOverlap.AddDynamic(this, &AMeleeWeapon::Prox);
+	ProxBox->AttachTo(RootComponent);
 }
 
 // Called when the game starts or when spawned
@@ -27,5 +37,27 @@ void AMeleeWeapon::Tick(float DeltaTime)
 
 void AMeleeWeapon::Prox_Implementation(class UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
 {
+	// Don't hit non-root components
+	if (OtherComp != OtherActor->GetRootComponent())
+	{
+		return;
+	}
+	// Avoid hitting when not swinging, self, and the same actor twice
+	if (Swinging && OtherActor != WeaponHolder && !ThingsHit.Contains(OtherActor))
+	{
+		OtherActor->TakeDamage(AttackDamage + WeaponHolder->BaseAttackDamage, FDamageEvent(), NULL, this);
+		ThingsHit.Add(OtherActor);
+	}
+}
 
+void AMeleeWeapon::Swing()
+{
+	ThingsHit.Empty();
+	Swinging = true;
+}
+
+void AMeleeWeapon::Rest()
+{
+	ThingsHit.Empty();
+	Swinging = false;
 }
--- Source/GoldenEgg/MeleeWeapon.h
+++ Source/GoldenEgg/MeleeWeapon.h
@@ -42,4 +42,6 @@ public:
 		void Prox(class UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult);
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
+	void Swing();
+	void Rest();
 };
